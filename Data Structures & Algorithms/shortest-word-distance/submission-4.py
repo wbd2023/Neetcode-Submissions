467f0edfe8c54cf7c7c8ab1cf8923eed86304678
William Duong <william.duong2023+github@gmail.com>
2026-08-18 14:53:11 +1000

    Add: shortest-word-distance - submission-4

---
+++ Data Structures & Algorithms/shortest-word-distance/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
+        best = 3 * 10**4 + 1
+
+        for i, word in enumerate(wordsDict):
+            if word != word1 and word != word2:
+                continue
+
+            target = word1 if word == word2 else word2
+            for j, candidate in enumerate(wordsDict[i + 1 :]):
+                if candidate == target:
+                    best = min(best, j + 1)
+                    break
+
+        return best
