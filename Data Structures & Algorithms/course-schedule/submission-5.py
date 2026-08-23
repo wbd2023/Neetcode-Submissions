f67d30399a336924ed4ec6acd3226e0bc12bb8c7
William Duong <william.duong2023+github@gmail.com>
2026-08-23 11:30:57 +1000

    Add: course-schedule - submission-5

---
+++ Data Structures & Algorithms/course-schedule/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        dependencies = [set() for _ in range(numCourses)]
+        dependents = [set() for _ in range(numCourses)]
+        for course, prerequisite in prerequisites:
+            dependencies[course].add(prerequisite)
+            dependents[prerequisite].add(course)
+
+        queue = deque()
+        for course in range(numCourses):
+            if not dependencies[course]:
+                queue.append(course)
+
+        completed = 0
+        while queue:
+            course = queue.popleft()
+            completed += 1
+
+            for dependent in dependents[course]:
+                dependencies[dependent].remove(course)
+
+                if not dependencies[dependent]:
+                    queue.append(dependent)
+
+        return completed == numCourses
