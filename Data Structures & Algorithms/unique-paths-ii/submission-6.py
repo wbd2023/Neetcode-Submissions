7a9f09ae52c7bb8780908df690a77ca0818bfc98
William Duong <william.duong2023+github@gmail.com>
2026-08-26 15:55:59 +1000

    Add: unique-paths-ii - submission-6

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-6.py
@@ -0,0 +1,21 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        if obstacleGrid[0][0] == 1 or obstacleGrid[m - 1][n - 1] == 1:
+            return 0
+
+        for r, line in enumerate(obstacleGrid):
+            for c, cell in enumerate(line):
+                if r == c == 0:
+                    obstacleGrid[r][c] = 1
+                    continue
+
+                if cell == 1:
+                    obstacleGrid[r][c] = 0
+                    continue
+
+                # Add from the relative left and top cells, respectively.
+                obstacleGrid[r][c] += obstacleGrid[r - 1][c] if 0 <= r - 1 < m else 0
+                obstacleGrid[r][c] += obstacleGrid[r][c - 1] if 0 <= c - 1 < n else 0
+
+        return obstacleGrid[m - 1][n - 1]
