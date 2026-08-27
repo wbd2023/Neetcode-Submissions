13a43e6d0eb2b00d9b8f1230d4992fabfee1d8d8
William Duong <william.duong2023+github@gmail.com>
2026-08-27 21:08:10 +1000

    Add: topologicalSort - submission-3

---
+++ Data Structures & Algorithms/topologicalSort/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def topologicalSort(self, n: int, edges: List[List[int]]) -> List[int]:
+        indegree = defaultdict(int)
+        adjacency = defaultdict(list)
+        for source, destination in edges:
+            indegree[destination] += 1
+            adjacency[source].append(destination)
+
+        queue = deque()
+        for node in range(n):
+            if indegree.get(node, 0) == 0:
+                queue.append(node)
+
+        result = []
+        while queue:
+            node = queue.popleft()
+            result.append(node)
+
+            for neighbour in adjacency.get(node, []):
+                indegree[neighbour] -= 1
+
+                if indegree[neighbour] == 0:
+                    queue.append(neighbour)
+
+        return result if len(result) == n else []
